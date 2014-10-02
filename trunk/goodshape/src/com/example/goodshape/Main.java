571bbf56c6f672db779a45717998d4fb1eb30378
dptmf762@gmail.com <dptmf762@gmail.com@89c170d0-798c-58bd-f31b-1e64d34372b6>
2014-10-02 08:48:31 +0000

    main and free menu update

--- trunk/goodshape/src/com/example/goodshape/Main.java
+++ trunk/goodshape/src/com/example/goodshape/Main.java
@@ -1,16 +1,59 @@
 package com.example.goodshape;
-
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class Main extends Activity {
 
+	ImageView logoImageview;
+	Button free_btn, recommend_btn,record_btn,profile_btn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		  
+		logoImageview = (ImageView)findViewById(R.id.imageView1);
+		free_btn = (Button)findViewById(R.id.free_btn);
+		recommend_btn = (Button)findViewById(R.id.recommend_btn);
+		record_btn = (Button)findViewById(R.id.record_btn);
+		profile_btn = (Button)findViewById(R.id.profile_btn);
+		  
+		free_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.free_menu);
+			}	
+		});
+		
+		recommend_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.free_menu);
+			}	
+		});
+		
+		record_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.free_menu);
+			}	
+		});
+		
+		record_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.free_menu);
+			}	
+		});
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
